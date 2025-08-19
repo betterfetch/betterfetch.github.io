8cec44b7424fc0612c5f4d47ae5a5f33d717fed9
David Balishyan <davidbalishyan12@gmail.com>
2025-08-19 14:19:12 +0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-// setTimeout(() => {
-//   window.location.href = "https://betterfetch.netlify.app";
-// }, 500);
+setTimeout(() => {
+  window.location.href = "https://betterfetch.netlify.app";
+}, 500);
